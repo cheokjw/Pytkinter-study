0f3f7acd63c0907e2538d1de30b129c16671b7c0
cheokjw <65707622+cheokjw@users.noreply.github.com>
2021-04-26 13:51:22 +0800

    Update tk image slides.py

--- tkinter/tk image slides.py
+++ tkinter/tk image slides.py
@@ -3,7 +3,7 @@ from PIL import ImageTk, Image
 
 #Initializing the GUI program
 root = Tk()
-root.title("image slides")
+root.title("Image slides")
 
 #Defining all the images by using the PIL lib
 my_img1 = ImageTk.PhotoImage(Image.open("C:/Users/cheok/OneDrive/Documents/GitHub/pytkinter-study/tkinter/images/PoliticalIdleItalianbrownbear-size_restricted.gif"))
@@ -15,6 +15,14 @@ my_img5 = ImageTk.PhotoImage(Image.open("C:/Users/cheok/OneDrive/Documents/GitHu
 
 #Listing all the images into an img_list
 img_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+
+#Defining the status bar
+#bd = border, (bd = 1 = enable border)
+#anchor = moving the label to the position according to S(South), E(East), N(North), W(West)
+#relief = the status/ look/ attribute of the border. It can be RAISED, SUNKEN, RIDGE, SOLID, GROOVE or FLAT
+status = Label(root, text = f"Image 1 of {len(img_list)}", bd = 1, relief = SUNKEN, anchor = E)
+
 
 #Showing the first/ default image onto the GUI program
 my_label = Label(image = my_img1)
@@ -33,7 +41,7 @@ def forward(img_index):
     my_label.grid_forget()
     my_label = Label(image = img_list[img_index])
 
-    #Plus 1 into the img_index to show the next image if the forward button is pressed. For example, if the default img_index is 0, it'll add 1 into img_index so it becomes 1
+    #Plus 1 into the img_index to show the next image if the forward button is pressed. For example, if the default img_index is 1, it'll add 1 into img_index so it becomes 2
     #Thus, it'll show the image which has the img_index of 1, which is, the second image.
     button_forward = Button(root, text = ">>>", command = lambda: forward(img_index + 1))
     button_backward = Button(root, text = "<<<", command = lambda: backward(img_index - 1))
@@ -48,7 +56,12 @@ def forward(img_index):
     button_backward.grid(row = 1, column = 0)
     button_forward.grid(row = 1, column = 2)
 
+    #Updating the status label according to the img_index
+    status = Label(root, text = f"Image {img_index + 1} of {len(img_list)}", bd = 1, relief = SUNKEN, anchor = E)
+    status.grid(row = 2, column = 0, columnspan = 3, sticky = W + E)
+
 #Basically the same concept as the forward func but its reversed
+#img_index variable is called from the updated button_backward from forward func
 def backward(img_index):
     global my_label
     global button_forward
@@ -66,6 +79,9 @@ def backward(img_index):
     button_backward.grid(row = 1, column = 0)
     button_forward.grid(row = 1, column = 2)
 
+    #Updating the status label according to the img_index
+    status = Label(root, text = f"Image {img_index + 1} of {len(img_list)}", bd = 1, relief = SUNKEN, anchor = E)
+    status.grid(row = 2, column = 0, columnspan = 3, sticky = W + E)
 
 
 #root.quit button command is for exiting the GUI program after pressing the button
@@ -77,8 +93,10 @@ button_forward = Button(root, text = ">>>", command = lambda: forward(1))
 
 button_back.grid(row = 1, column = 0)
 button_exit.grid(row = 1, column = 1)
-button_forward.grid(row = 1, column = 2)
-
+button_forward.grid(row = 1, column = 2, pady = 10)
+#sticky = S(South)/E(East)/N(North)/W(West) = To stretch the grid to any position
+#sticky = W + E means stretching the grid from left to right / N + W means stretching from up to down
+status.grid(row = 2, column = 0, columnspan = 3, sticky = W + E)
 
 
 
